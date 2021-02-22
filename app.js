3b2f95b46938ed079079a50dc02c9dcd5bf0fdd8
Eddie <github.towage@aleeas.com>
2021-02-21 21:54:45 -0800

    refactor

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js")
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = []
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,20 +12,33 @@ app.use(express.static("public"))
 
 app.get('/', function(req, res) {
 
-  let today = new Date();
-  let options = {weekday:"long", day: "numeric", month:"long"}
+  let day = date.getDate()
 
-  let day = today.toLocaleDateString("en-US", options)
-
-  res.render("list", {kindOfDay:day, newListItems: items  })
+  res.render("list", {listTitle:day, newListItems: items  })
 
 app.post("/", function(req, res) {
   let item = req.body.newItem
 
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+
   items.push(item);
   res.redirect("/")
+}
 })
 
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+
+})
+
+app.post("/work", function(req,res){
+  let item = req.body.newItem
+  workItems.push(item)
+  res.redirect("/work")
+})
 
 
 })
